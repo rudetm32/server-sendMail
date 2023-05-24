b86789c1bdcb7d73053148818f94c8f835d0bbf3
rudetm32 <jaruiz721818@gmail.com>
2023-05-23 22:19:02 -0600

    fix(src/componets/Home.jsx): formulario controlado

--- client/src/componets/Home.jsx
+++ client/src/componets/Home.jsx
@@ -1,49 +1,101 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import Swal from "sweetalert2";
+import TextField from '@mui/material/TextField'
+import { Button } from '@mui/material';
+
+import style from "./Home.module.css"
 
 export default function Home() {
-  return (
+
+  const { register, reset, formState : { errors, isSubmitSuccessful }, handleSubmit } =  useForm({
+  defaultValues :{}
+});
+
+useEffect(() => {
+  if (isSubmitSuccessful) { 
+    reset({ name: "", address: "", email: "" });
+  }
+}, [ isSubmitSuccessful, reset ]);
+
+const onSubmit = (data) => {
+  axios
+  .post("http://localhost:3001/send", data)
+  .then((data) => {
+    if (data) {Swal.fire({
+          icon: 'success',
+          title: 'Se envio una notificacion al correo registrado',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Ha ocurrido un error al registrar el usuario',
+          text: 'Por favor, inténtelo de nuevo',
+          confirmButtonText: 'OK'
+        })
+      }
+    })
+    .catch((error) => {
+      Swal.fire({
+      icon: 'warning',
+      title: 'correo incorrecto',
+      text: error,
+      confirmButtonText: 'OK'
+    });
+  });
+};
+
+return(
     <>
-    <h1>Formulario de registro</h1>
-    <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div className='conteiner'>
-      <div>
-      <TextField
-          required
-          id="filled-required"
-          label="Nombre"
-          variant="filled"
-        />
-          </div>
-          <div>
-          <TextField
-          required
-          id="filled-required"
-          label="Direccion"
-          variant="filled"
-        />  
-          </div>
-      <div > 
-        <TextField
-          required
-          id="filled-required"
-          label="Correo Electronico"
-          variant="filled"
-        />
-      </div>
-      </div>
-      
-      
-    </Box>
-    </>
-    
-  );
-}
+    <div className={style.conteiner}>
+      <form p={2} 
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }} onSubmit= { handleSubmit(onSubmit) } >
+            <div><h3 >Formulario de Registro</h3></div>
+            
+            <div >
+              <TextField
+                required
+                id = "message-name"
+                type="text" {...register("name", {required: true, maxLength:20})}
+                label = "Nombre"
+                variant = "standard"
+              />
+                {errors.name?.type==="maxLength" && <p >campo requerido</p>}
+            </div>
+            <br />
+            <div>
+                <TextField
+                  id = "message-address"
+                  type="text" {...register("address", {required: false, maxLength:20})}
+                  label = "Direccion"
+                  variant = "standard"
+                />
+                  {errors.address?.type==="maxLength" && <p >ingrese una direccion</p>}
+            </div>
+            <br />
+            <div>
+                <TextField
+                  required
+                  id = "message-email"
+                  type="email" {...register("email", {required: true})}
+                  label = "Correo electronico"
+                  variant = "standard"
+                />
+                  {errors.email?.type==="maxLength" && <p >Ingrese un correo valido</p>}
+            </div>
+            <br />
+            <div>
+              <Button  type="submit" variant="contained" color="success">
+                Enviar
+              </Button>
+            </div>
+      </form>
+    </div>
+  </>
+  )
+};
